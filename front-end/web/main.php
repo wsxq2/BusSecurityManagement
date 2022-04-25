6da4585f7661c087d6783d23108ffc088ef4e4a8
wsxq2 <wsxq2@qq.com>
2022-04-25 19:50:35 +0800

    fix(front-end): add utf-8 head option

--- front-end/web/main.php
+++ front-end/web/main.php
@@ -11,6 +11,7 @@ if( !isset($_SESSION['uname']) ){
 ?>
 <!DOCTYPE html>
 <html>
+<meta charset="UTF-8">
 <title>公交安全管理系统</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="css/w3.css">
